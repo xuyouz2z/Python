672d94b53fb6890e9b7ed6b9a02431c387be9f9a
xuyouz2z <xubaibai922@gmail.com>
2022-12-23 20:10:58 +0800

    20221223

--- FC/004.py
+++ FC/004.py
@@ -3,3 +3,23 @@ print("les's go")
 print('les\'s go')
 print('''les's
 go!go!''')
+
+print('-'*20)
+
+print('C:\now')
+# 进行转义
+print('C:\\now')
+
+# 原始字符串（需在引号之前添加r）
+str = r'C:\now\FC'
+print(str)
+# 注意：原始字符串末尾不能加反斜杠\
+str1 = r'C:\now\FC''\\'
+print(str1)
+print(r'C:\now\FC\test''\\')
+
+print('-'*20)
+
+print('''你好
+世界
+哈哈哈''')
