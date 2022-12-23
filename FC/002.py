3b2a84525a92dddc6ec010fc641ef0b73844faea
xuyouz2z <xubaibai922@gmail.com>
2022-12-23 17:53:28 +0800

    001.002

--- FC/002.py
+++ FC/002.py
@@ -1,3 +1,6 @@
-print(f'1+2={1+2}')
-print('hello'+' world')
-print('你好','Python')
+temp = int(input('不妨猜一下我心里想的什么数？'))
+if temp == 8:
+    print('猜对啦~，猜对也没有奖励哈哈哈哈')
+else:
+    print('猜错啦，也就这样嘛~')
+print('谢谢参与')
