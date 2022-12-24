488ac698782da0b0bc4b6e1167bbef9d529d0183
xuyouz2z <xubaibai922@gmail.com>
2022-12-24 12:07:57 +0800

    20221224

--- FC/004.py
+++ FC/004.py
@@ -1,25 +1,21 @@
-print("les's go")
-# 进行转义
-print('les\'s go')
-print('''les's
-go!go!''')
-
-print('-'*20)
-
-print('C:\now')
-# 进行转义
-print('C:\\now')
-
+# 横向制表符
+print('1\t23')
+# 纵向制表符
+print('1\v23')
+# 换行
+print('12\n3')
+# 输出带'的
+print("Les's go")
+print('Les\'s go')
 # 原始字符串（需在引号之前添加r）
-str = r'C:\now\FC'
-print(str)
-# 注意：原始字符串末尾不能加反斜杠\
-str1 = r'C:\now\FC''\\'
+str1 = r'D:\Python\now'
 print(str1)
-print(r'C:\now\FC\test''\\')
-
-print('-'*20)
-
+# 注意：原始字符串末尾不能加反斜杠\
+# print(r'D:\Python\now\')
+str2 = r'D:\Python\now''\\'
+print(str2)
+print(r'D:\Python\now''\\')
+# 三引号（无需换行）
 print('''你好
 世界
 哈哈哈''')
