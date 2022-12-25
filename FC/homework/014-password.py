3b8f6e823223824aa6b6e15ed0e94f8303b3f604
xuyouz2z <xubaibai922@gmail.com>
2022-12-25 16:44:42 +0800

    20221225

--- FC/homework/014-password.py
+++ FC/homework/014-password.py
@@ -1,7 +1,17 @@
-for i in range(3):
-    password = int(input('请输入密码：'))
-    if password == 88888888:
+password = 8888
+# 输入次数
+count = 3
+while count:
+    user = input('请输入密码：')
+    if user == password:
         print('输入正确')
         break
+    elif '*' in user:
+        print(f'密码中不能含有“*”号，请重新输入，您还有{count}机会')
+        # continue跳过循环（后边的语句不执行）
+        continue
+    elif count == 1:
+        print('输入错误，请稍后再试')
     else:
-        print(f'请重新输入，还有{i}次机会')
+        print(f'请重新输入，还有{count-1}次机会')
+    count -= 1
