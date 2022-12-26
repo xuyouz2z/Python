c0d698ee9ba777f76fcf8233a4de9931fb2f66ec
xuyouz2z <xubaibai922@gmail.com>
2022-12-26 22:06:07 +0800

    20221226

--- FC/class/016-for.py
+++ FC/class/016-for.py
@@ -2,7 +2,7 @@ talk = 'ILOVEYOU'
 for i in talk:
     print(i,end='')
 print('')
-number = ['apple','banana','tangerine']
+number = ['apple','banana','orange']
 for i in number:
     print(i,len(i))
 print(list(range(5)))
--- FC/homework/014-password.py
+++ FC/homework/014-password.py
@@ -1,17 +1,16 @@
-password = 8888
-# 输入次数
 count = 3
+password = '888'
+
 while count:
     user = input('请输入密码：')
     if user == password:
-        print('输入正确')
+        print('密码正确！')
         break
     elif '*' in user:
-        print(f'密码中不能含有“*”号，请重新输入，您还有{count}机会')
-        # continue跳过循环（后边的语句不执行）
+        print('密码中不能含有"*"号！您还有', count, '次机会！')
         continue
     elif count == 1:
-        print('输入错误，请稍后再试')
+        print('请稍后再试')
     else:
-        print(f'请重新输入，还有{count-1}次机会')
+        print('密码输入错误！您还有', count-1, '次机会！')   
     count -= 1
